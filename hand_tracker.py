d2fd97c002e2f82a86764c96827372b0292198f1
Adith1207 <adithnarayang@gmail.com>
2025-09-08 19:27:33 +0530

    added find_hands function to detect hands

--- hand_tracker.py
+++ hand_tracker.py
@@ -12,3 +12,19 @@ class HandTracker:
         )
         self.mp_draw = mp.solutions.drawing_utils
         self.locked_hand_landmarks = None
+    
+    def find_hands(self, frame, draw=True):
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(rgb_frame)
+
+        if self.results.multi_hand_landmarks:
+            self.locked_hand_landmarks = self.results.multi_hand_landmarks[0]
+
+            if draw:
+                self.mp_draw.draw_landmarks(
+                    frame, self.locked_hand_landmarks, self.mp_hands.HAND_CONNECTIONS
+                )
+        else:
+            self.locked_hand_landmarks = None
+
+        return frame
