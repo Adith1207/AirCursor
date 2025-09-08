8f3afcf5419fc951a53a6f3c31581f0b7c0f5e7c
Adith1207 <adithnarayang@gmail.com>
2025-09-08 19:25:45 +0530

    added HandTracker class & its constructor

--- hand_tracker.py
+++ hand_tracker.py
@@ -1,2 +1,14 @@
 import cv2
 import mediapipe as mp
+
+class HandTracker:
+    def __init__(self, max_hands=1, detection_conf=0.7, track_conf=0.7):
+        self.max_hands = max_hands
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(
+            max_num_hands=self.max_hands,
+            min_detection_confidence=detection_conf,
+            min_tracking_confidence=track_conf
+        )
+        self.mp_draw = mp.solutions.drawing_utils
+        self.locked_hand_landmarks = None
