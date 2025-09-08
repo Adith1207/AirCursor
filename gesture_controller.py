3d18ffc65d4406fa1f94d98389e2badf5a4ed283
Adith1207 <adithnarayang@gmail.com>
2025-09-08 19:08:50 +0530

    Added class GestureController

--- gesture_controller.py
+++ gesture_controller.py
@@ -2,3 +2,10 @@ import math
 import pyautogui
 
 SCREEN_WIDTH, SCREEN_HEIGHT = pyautogui.size()
+
+class GestureController:
+    def __init__(self, sensitivity=2.5, dead_zone=5):
+        self.prev_finger_pos = None
+        self.sensitivity = sensitivity
+        self.dead_zone = dead_zone
+        self.dragging = False
