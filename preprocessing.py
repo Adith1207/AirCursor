13aad210c19a70c96b21e1210648b11dd9454b86
Adith1207 <adithnarayang@gmail.com>
2025-09-12 21:10:13 +0530

    added webcamStream class with its constructor to capture image from webcam

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,15 @@
 import cv2
 import numpy as np
 import math
+
+
+class WebcamStream:
+    def __init__(self, camindex=0, multi_hand=False):
+        self.camindex = camindex
+        self.capture = cv2.VideoCapture(self.camindex)
+
+        if not self.capture.isOpened():
+            raise Exception("Webcam not Opened")
+
+        self.kernel = np.ones((5, 5), np.uint8)
+        self.multi_hand = multi_hand  
