21ded703fc1c10662fe9bfc4e0166de4b2cb15b2
Adith1207 <adithnarayang@gmail.com>
2025-09-12 23:32:28 +0530

    added constants for CSV files & joblibs files

--- train_model.py
+++ train_model.py
@@ -5,3 +5,8 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, accuracy_score
+
+
+CSV_FILE = "gesture_dataset.csv"
+MODEL_FILE = "gesture_model.joblib"
+LE_FILE = "label_encoder.joblib"
